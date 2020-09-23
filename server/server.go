5b6cc7e1b08b36bb831e97137be7695a8411e14f
miyazi777 <m71203@gmail.com>
2020-09-23 22:14:47 +0900

    add custom interceptor

--- server/server.go
+++ server/server.go
@@ -15,7 +15,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(CustomInterceptor()),
+	)
 	hello.RegisterHelloServer(server, &Hello{})
 	server.Serve(listenPort)
 }
@@ -23,6 +25,24 @@ func main() {
 type Hello struct{}
 
 func (h *Hello) Hello(cts context.Context, message *hello.HelloMessage) (*hello.HelloResponse, error) {
-	res := hello.HelloResponse{Msg: fmt.Sprintf("hello %s\n", message.Name)}
+	res := hello.HelloResponse{Msg: fmt.Sprintf("hello %s", message.Name)}
 	return &res, nil
+}
+
+func CustomInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, request interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		fmt.Printf("before process")
+		helloMessage, _ := request.(*hello.HelloMessage)
+		helloMessage.Name = fmt.Sprintf("<<< %s >>>", helloMessage.Name)
+
+		res, err := handler(ctx, request)
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Println("after process")
+		helloResponse, _ := res.(*hello.HelloResponse)
+		helloResponse.Msg = fmt.Sprintf("*** %s ***", helloResponse.Msg)
+		return res, nil
+	}
 }
